396c95212ae38e622cfe58445e757530dbec4355
qiaoxiangxiang <qiaoxiangxiang0123@163.com>
2019-04-06 13:55:31 +0800

    ceshi

---
+++ DataType.java
@@ -0,0 +1,7 @@
+class DataType 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+	}
+}
---
+++ Java_Demo1.java
@@ -0,0 +1,10 @@
+class Java_Demo1 {
+	public static void main (String[] args){
+		//你好
+		System.out.println(121212);
+		System.out.println(0x100);
+		System.out.println("hello world");
+		System.out.println(0100);
+		System.out.println(0562);
+	}
+}
